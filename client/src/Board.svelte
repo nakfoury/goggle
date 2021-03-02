fd5c09eaf4f21571b910172a4676820b9b6d2445
neal <nakfoury@gmail.com>
2021-03-01 22:05:59 -0800

    Stub a board. Host=0.0.0.0

---
+++ client/src/Board.svelte
@@ -0,0 +1,22 @@
+<script lang="typescript">
+  import { Table } from 'svelte-materialify/src'
+
+  export let size = 4
+</script>
+
+<Table dense class="board">
+  {#each { length: size } as x}
+    <tr>
+      {#each { length: size } as y}
+        <td>A</td>
+      {/each}
+    </tr>
+  {/each}
+</Table>
+
+<style lang="scss">
+  .board {
+    font-size: 100pt;
+    background: blue;
+  }
+</style>
